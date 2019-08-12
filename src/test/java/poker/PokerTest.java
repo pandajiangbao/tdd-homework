9c6eb99c2758b551d66351c13278b46cd12e0d70
WUPA2 <panda.wu@oocl.com>
2019-08-12 19:40:19 +0800

    finish testPoker_given_two_pokers_then_compare_one_card_size_when_one_pokers_is_THREE_OF_A_KIND_and_another_pokers_is_TWO_PAIR

--- src/test/java/poker/PokerTest.java
+++ src/test/java/poker/PokerTest.java
@@ -39,7 +39,7 @@ public class PokerTest {
     }
 
     @Test
-    public void testPoker_given_two_pokers_then_compare_one_card_size_when_one_pokers_is_pair_and_another_pokers_is_high_card(){
+    public void testPoker_given_two_pokers_then_compare_one_card_size_when_one_pokers_is_PAIR_and_another_pokers_is_HIGH_CARD(){
         List<Poker> player1 = Arrays.asList(new Poker("C","5"), new Poker("D","A"), new Poker("H","K"), new Poker("H","8"), new Poker("S","5"));
         List<Poker> player2 = Arrays.asList(new Poker("C","A"), new Poker("D","5"), new Poker("C","K"), new Poker("H","Q"), new Poker("H","9"));
 
@@ -50,9 +50,31 @@ public class PokerTest {
     }
 
     @Test
-    public void testPoker_given_two_pokers_then_compare_one_card_size_when_one_pokers_is_pair_and_another_pokers_is_two_pair(){
+    public void testPoker_given_two_pokers_then_compare_one_card_size_when_one_pokers_is_pair_and_another_pokers_is_TWO_PAIR(){
         List<Poker> player1 = Arrays.asList(new Poker("C","5"), new Poker("D","A"), new Poker("H","K"), new Poker("H","8"), new Poker("S","5"));
         List<Poker> player2 = Arrays.asList(new Poker("C","2"), new Poker("D","2"), new Poker("S","4"), new Poker("C","4"), new Poker("H","9"));
+
+
+        String result= Poker.compareCards(player1,player2);
+
+        Assert.assertEquals("player2 win",result);
+    }
+
+    @Test
+    public void testPoker_given_two_pokers_then_compare_one_card_size_when_one_pokers_is_THREE_OF_A_KIND_and_another_pokers_is_TWO_PAIR(){
+        List<Poker> player1 = Arrays.asList(new Poker("C","5"), new Poker("D","A"), new Poker("H","5"), new Poker("H","8"), new Poker("S","5"));
+        List<Poker> player2 = Arrays.asList(new Poker("C","2"), new Poker("D","2"), new Poker("S","4"), new Poker("C","4"), new Poker("H","9"));
+
+
+        String result= Poker.compareCards(player1,player2);
+
+        Assert.assertEquals("player1 win",result);
+    }
+
+    @Test
+    public void testPoker_given_two_pokers_then_compare_one_card_size_when_one_pokers_is_THREE_OF_A_KIND_and_another_pokers_is_STRAIGHT(){
+        List<Poker> player1 = Arrays.asList(new Poker("C","5"), new Poker("D","A"), new Poker("H","5"), new Poker("H","8"), new Poker("S","5"));
+        List<Poker> player2 = Arrays.asList(new Poker("C","2"), new Poker("D","3"), new Poker("S","4"), new Poker("D","5"), new Poker("H","6"));
 
 
         String result= Poker.compareCards(player1,player2);
